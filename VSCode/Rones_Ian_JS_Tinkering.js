30d42785cee8c710616a2db9626961d099695f13
Prones94 <ronesp.85@gmail.com>
2020-09-05 20:50:45 -0700

    [add] tinkering js file

---
+++ VSCode/Rones_Ian_JS_Tinkering.js
@@ -0,0 +1,9 @@
+let childheight = 10;
+
+const displayIfChildIsAbleToRideTheRollerCoaster = () => {
+    if(childheight > 52) {
+        console.log('Get on the ride, kiddo!');
+    } else {
+        console.log('Sorry, kiddo. Maybe next year.');
+    }
+}
