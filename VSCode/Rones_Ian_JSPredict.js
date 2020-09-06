fe4ffd6fe3ce63577201c11563f9af81e4740b0b
Prones94 <ronesp.85@gmail.com>
2020-09-05 19:32:01 -0700

    [add] jspredict file

---
+++ VSCode/Rones_Ian_JSPredict.js
@@ -0,0 +1,24 @@
+const myBirthYearFunc = (year) => {
+    console.log(`I was born in ${year}`);
+}
+// I was born in 1994
+
+
+
+const add = (num1, num2) => {
+    console.log("Summing Numbers!");
+    console.log(`num1 is ${num1}`);
+    console.log(`num2 is ${num2}`);
+    let sum = num1 + num2;
+    console.log(sum);
+}
+
+/*
+ Summing Numbers!
+ num1 is 10
+ num2 is 20
+ 30
+ */
+
+myBirthYearFunc(1994)
+add(10,20);
